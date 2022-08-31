f9bc652ef1b520c2d3a1e0b60d746a244975ac28
Mauro Erben <mauroerben100@hotmail.com>
2022-08-31 15:46:08 -0300

    se agrego la ruta para crear post

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NewPost from './pages/CreatePost'
+
 function App() {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/create-post" element={<NewPost />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App
